df78cf15801f7b25c391a10f7d01c2ba52a43e91
Amber Taranto <ambzzzz@gmail.com>
2019-06-02 23:24:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(seconds)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  number = seconds
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
